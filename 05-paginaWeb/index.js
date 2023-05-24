4ef548408ea1871edc30f68387c9a8e4d0db4656
ALANMer <alanmercadoamg@gmail.com>
2023-05-24 04:25:40 -0600

    Documentacion

--- 05-paginaWeb/index.js
+++ 05-paginaWeb/index.js
@@ -1,3 +1,9 @@
+/* Este código agrega un detector de eventos al objeto de ventana que escucha el evento
+"desplazamiento". Cuando el usuario se desplaza, el código comprueba la posición de desplazamiento
+actual y, si es superior a 300 píxeles, muestra un botón de "desplazamiento hacia arriba". Cuando el
+usuario hace clic en el botón "desplazarse hacia arriba", desplaza la ventana hacia la parte
+superior de la página con una animación suave. */
+
 window.addEventListener("scroll", function () {
   const scrollPosition =
     window.pageYOffset || document.documentElement.scrollTop;
